679f329f252ed659eacdcd3b57615bf1d647459c
user <user@NC34A02906>
2025-11-20 15:19:28 +0800

    加入專案檔案。

---
+++ FluentUI/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace FluentUI
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btn3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btn4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btn5_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btn6_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btn7_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btn8_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btn9_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btn0_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            ((Button)sender).Content = "쨢헕을좮";
+        }
+        private void btnMinus_Click(object sender, RoutedEventArgs e)
+        {
+            ((Button)sender).Content = "쨢헕을좮";
+        }
+        private void btnPlus_Click(object sender, RoutedEventArgs e)
+        {
+            ((Button)sender).Content = "쨢헕을좮";
+        }private void btnDivide_Click(object sender, RoutedEventArgs e)
+        {
+            ((Button)sender).Content = "쨢헕을좮";
+        }
+    }
+}
